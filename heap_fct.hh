1ccaab84efdd821d7a7a118eb3823cea6b5d0a31
Raphael Treglia <raphael.treglia@epita.fr>
2019-06-27 10:54:43 +0200

    [MISC] rename file

--- heap_fct.hh
+++ heap_fct.hh
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "softheap.hh"
 #include <iostream>
 #include <cassert>
@@ -391,39 +393,3 @@ bool SoftHeap<E>::deleteE(E e) {
 }
 
 
-int main() {
-    SoftHeap<int> *s = new SoftHeap<int>(5);
-    s->insert(3);
-    s->insert(4);
-    s->insert(6);
-    s->insert(7);
-    s->insert(8);
-    /*s->insert(2);
-    s->insert(1);
-    s->insert(9);*/
-
-    /*std::cout << "DELETE 6: " << s->deleteE(6) << std::endl;
-    std::cout << "DELETE 3: " << s->deleteE(3) << std::endl;
-    std::cout << "DELETE 4: " << s->deleteE(4) << std::endl;
-    std::cout << "DELETE 5: " << s->deleteE(5) << std::endl;*/
-
-
-
-    SoftHeap<int> *nw = new SoftHeap<int>(2);
-    nw->insert(1);
-    nw->insert(9);
-
-    s->meld(nw);
-
-    std::cout << s->extract_min() << std::endl;
-    std::cout << s->extract_min() << std::endl;
-    std::cout << s->extract_min() << std::endl;
-    std::cout << s->extract_min() << std::endl;
-    std::cout << s->extract_min() << std::endl;
-    std::cout << s->extract_min() << std::endl;
-    std::cout << s->extract_min() << std::endl;
-    std::cout << s->extract_min() << std::endl;
-    std::cout << s->extract_min() << std::endl;
-
-    delete s;
-}
