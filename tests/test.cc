f432bd547cbde84ed8dfb02176d9239d0e773759
Ferial Malki <ferial.malki@epita.fr>
2019-06-28 16:30:05 +0200

    [test.cc] hard meld failed

--- tests/test.cc
+++ tests/test.cc
@@ -6,78 +6,116 @@ class Test
     public:
         static void unit_test_first_ckey()
         {
-            SoftHeap<int> *s = new SoftHeap(3);
-            s->insert(6);
-            s->insert(4);
-            assert(s->first->root->ckey == 4);
-
+            int a = 3, b = 6, c = 4;
+            SoftHeap<int> *s = new SoftHeap(&a);
+            s->insert(&b);
+            s->insert(&c);
+            assert(*s->first->root->ckey == 4);
+            std::cout<<"pine\n";
             delete s;
         }
 
         static void unit_test_next_ckey()
         {
-            SoftHeap<int> *s = new SoftHeap(3);
-            s->insert(6);
-            s->insert(4);
-            assert(s->first->next->root->ckey == 3);
+            int a = 3, b = 6, c = 4;
+            SoftHeap<int> *s = new SoftHeap(&a);
+            s->insert(&b);
+            s->insert(&c);
+            assert(*s->first->next->root->ckey == 3);
             delete s;
         }
 
         static void unit_test_child_ckeys()
         {
-            SoftHeap<int> *s = new SoftHeap(3);
-            s->insert(6);
-            s->insert(4);
-            s->insert(12);
-            s->insert(1);
-            s->insert(50);
+            int a = 3, b = 6, c = 4, d = 12, e = 1, f = 50;
+            SoftHeap<int> *s = new SoftHeap(&a);
+            s->insert(&b);
+            s->insert(&c);
+            s->insert(&d);
+            s->insert(&e);
+            s->insert(&f);
 
-            assert(s->first->root->ckey == 1);
-            assert(s->first->root->right->ckey == 50);
+            assert(*s->first->root->ckey == 1);
+            assert(*s->first->root->right->ckey == 50);
 
-            assert(s->first->next->root->ckey == 3);
-            assert(s->first->next->root->right->ckey == 4);
-            assert(s->first->next->root->left->ckey == 6);
-            assert(s->first->next->root->right->right->ckey == 12);
+            assert(*s->first->next->root->ckey == 3);
+            assert(*s->first->next->root->right->ckey == 4);
+            assert(*s->first->next->root->left->ckey == 6);
+            assert(*s->first->next->root->right->right->ckey == 12);
             delete s;
         }
 
         static void unit_test_suf_min()
         {
-            SoftHeap<int> *s = new SoftHeap(-9);
-            s->insert(-10);
-            s->insert(2);
+            int a = -9, b = -10, c = 2;
+            SoftHeap<int> *s = new SoftHeap(&a);
+            s->insert(&b);
+            s->insert(&c);
 
-            assert(s->extract_min() == -10);
+            assert(*s->extract_min().value() == -10);
             delete s;
         }
 
         static void unit_test_delete()
         {
-            SoftHeap<int> *s = new SoftHeap(2);
-            s->insert(9);
-            s->insert(1);
-            assert(s->extract_min() == 1);
-            assert(s->extract_min() == 2);
-            assert(s->extract_min() == 9);
+            int a = 2, b = 9, c = 1;
+            SoftHeap<int> *s = new SoftHeap(&a);
+            s->insert(&b);
+            s->insert(&c);
+            assert(*s->extract_min().value() == 1);
+            assert(*s->extract_min().value() == 2);
+            assert(*s->extract_min().value() == 9);
             assert(s->extract_min() == std::nullopt);
             delete s;
         }
 
         static void unit_test_meld()
         {
-            SoftHeap<int> *s = new SoftHeap(5);
-            s->insert(2);
+            int a = 5, b = 2, c = 3, d = 8, e = 1;
+            SoftHeap<int> *s = new SoftHeap(&a);
+            s->insert(&b);
 
-            SoftHeap<int> *p = new SoftHeap(3);
-            p->insert(8);
-            p->insert(1);
+            SoftHeap<int> *p = new SoftHeap(&c);
+            p->insert(&d);
+            p->insert(&e);
 
             s->meld(p);
 
-            assert(s->extract_min() == 1);
+            assert(*s->extract_min().value() == 1);
             delete p;
             delete s;
+        }
+
+        static void unit_test_hard_meld()
+        {
+            int a = 6, b = 3, c = 12, d = 9, e = 19, f = 3, g = 8, h = 1,
+                i = 19, j = -1;
+            SoftHeap<int> *s = new SoftHeap(&a);
+            s->insert(&b);
+            s->insert(&c);
+            s->insert(&d);
+            s->insert(&e);
+
+            SoftHeap<int> *p = new SoftHeap(&f);
+            p->insert(&g);
+            p->insert(&h);
+            p->insert(&i);
+            p->insert(&j);
+
+            s->meld(p);
+
+            if ( *s->extract_min().value()== -1)
+                std::cout<< *s->extract_min().value();
+            if (*s->extract_min().value() == 1)
+                std::cout<<"non\n";
+            /*assert(*s->extract_min().value() == 3);
+            assert(*s->extract_min().value() == 3);
+            assert(*s->extract_min().value() == 6);
+            assert(*s->extract_min().value() == 8);
+            assert(*s->extract_min().value() == 9);
+            assert(*s->extract_min().value() == 12);
+            assert(*s->extract_min().value() == 19);
+            assert(*s->extract_min().value() == 19);*/
         }
 };
 
@@ -89,4 +127,5 @@ int main(void)
     Test::unit_test_suf_min();
     Test::unit_test_delete();
     Test::unit_test_meld();
+    Test::unit_test_hard_meld();
 }
