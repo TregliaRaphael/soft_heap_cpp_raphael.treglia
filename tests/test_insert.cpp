6a5d931a2e89cf3e6d2b2ef928e7549e705c1215
Aminata Faye <aminata.faye@epita>
2019-06-28 18:53:12 +0200

    [TESTS] add of a test

--- tests/test_insert.cpp
+++ tests/test_insert.cpp
@@ -18,7 +18,7 @@ void test_insert_tree()
   SoftHeap<int>::Tree *to = new SoftHeap<int>::Tree(&c);
   sh->insert_tree(ch,to,ti);
 
-  if (*sh->first->root->ckey != 60  || *sh->first->root->ckey != 10 || *sh->first->root->ckey != 14)
+  if (*sh->first->root->ckey != 60  || *sh->first->root->ckey != 10 || *sh->first->root->ckey != 14 || *sh->first->root->ckey != 4)
     {
       printf("\033[1;31m");
       printf("FAIL\n");
